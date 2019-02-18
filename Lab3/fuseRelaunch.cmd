bec20106a37c0da1878b46f9c04abbdc63e0eab9
John Adams <johnadams@mail.usf.edu>
2019-02-18 06:54:44 -0500

    John L4 init

--- Lab3/fuseRelaunch.cmd
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "G:/My Drive/Classes/CSD_Lab/csdlabs/Lab3/T0_isim_beh.exe" -prj "G:/My Drive/Classes/CSD_Lab/csdlabs/Lab3/T0_beh.prj" "work.T0" "work.glbl" 
-=======
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/Google Drive/Repos/csdlabs/Lab3/T0_isim_beh.exe" -prj "D:/Google Drive/Repos/csdlabs/Lab3/T0_beh.prj" "work.T0" "work.glbl" 
->>>>>>> JohnL3
