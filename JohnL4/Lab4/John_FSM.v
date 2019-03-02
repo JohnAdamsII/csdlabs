a0e977b72157a0c854aa8777ffc5664bcf2a3f1d
John Adams <johnadams@mail.usf.edu>
2019-03-01 22:39:28 -0500

    FSM.v and T1.v started coding FSM currently functions with 2 states

---
+++ JohnL4/Lab4/John_FSM.v
@@ -0,0 +1,82 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:43:13 03/01/2019 
+// Design Name: 
+// Module Name:    John_FSM 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module John_FSM(N_in,D_in,Q_in,diet_in,soda_in,GiveDiet,GiveSoda,clk,reset,N_out, D_out, Q_out);
+//Mealy Template:
+
+
+// inputs
+input clk, reset, N_in,D_in,Q_in, soda_in, diet_in;
+
+//outputs
+output reg GiveDiet, GiveSoda, N_out, D_out, Q_out; 
+
+
+parameter S0 = 2'b00;
+parameter S1 = 2'b01;
+parameter S2 = 2'b10;
+parameter S3 = 2'b11;
+
+
+(* F5M_ENCODING="SEQUNTIAL", SAFE_IMPLEMENTATION="NO" *)
+ reg [1:0] state = S0;
+ 
+ 
+always@(posedge clk )
+	if (reset) begin
+		state <= S0;
+	end
+	
+	else
+		(* FULL_CASE, PARALLEL_CASE *) 
+		
+		case (state)
+		   S0 : begin
+			
+			GiveDiet = 0;
+			GiveSoda = 0;
+			N_out = 0;
+			D_out = 0;
+			Q_out = 0; 
+			
+			if (N_in) 
+				state <= S1;
+				
+			else if (!N_in)
+				state <= S0;
+			else
+				state <= S0;
+		    end
+			 
+		   S1 : begin
+			
+		 N_out = 1;
+		 GiveSoda = 1;
+		 state <= S0;
+		 
+		    end
+			
+		endcase
+
+//assign <outputl> = <logic_equation_based_on_states_and;inputs>;
+//assign <output2> = <logic_equation_based_on_states_and;inputs>;
+// Add other output equations as necessary
+
+endmodule
---
+++ JohnL4/Lab4/T1.v
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   21:55:38 03/01/2019
+// Design Name:   John_FSM
+// Module Name:   G:/My Drive/Classes/CSD_Lab/Labs/Lab4/T1.v
+// Project Name:  Lab4
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: John_FSM
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module T1;
+
+	// Inputs
+	reg N_in;
+	reg D_in;
+	reg Q_in;
+	reg diet_in;
+	reg soda_in;
+	reg clk;
+	reg reset;
+
+	// Outputs
+	wire GiveDiet;
+	wire GiveSoda;
+	wire N_out;
+	wire D_out;
+	wire Q_out;
+
+	// Instantiate the Unit Under Test (UUT)
+	John_FSM uut (
+		.N_in(N_in), 
+		.D_in(D_in), 
+		.Q_in(Q_in), 
+		.diet_in(diet_in), 
+		.soda_in(soda_in), 
+		.GiveDiet(GiveDiet), 
+		.GiveSoda(GiveSoda), 
+		.clk(clk), 
+		.reset(reset), 
+		.N_out(N_out), 
+		.D_out(D_out), 
+		.Q_out(Q_out)
+	);
+
+
+
+	always begin
+	clk = ~clk;
+	#5;
+	end
+
+
+
+
+	initial begin
+	
+
+	
+		// Initialize Inputs
+		N_in = 0;
+		D_in = 0;
+		Q_in = 0;
+		diet_in = 0;
+		soda_in = 0;
+		clk = 0;
+		reset = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+		N_in = 1;
+
+	end
+      
+endmodule
+
